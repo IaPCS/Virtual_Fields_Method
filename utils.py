42dd62c4ca8c5858c7d60662c3702f5b4fcfdcb6
Ilyass Tabiai <joydisee@gmail.com>
2017-05-30 16:19:47 -0400

    Deck and Coordinates classes are ready!

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import yaml, os, sys
+import numpy as np
+import numpy.ma as ma
 
 class Deck():
 
@@ -32,6 +34,8 @@ class Deck():
         else:
             self.E = self.check_presence("Material", "E") 
             self.nu = self.check_presence("Material", "nu")    
+            self.Q11 = self.E / (1-(self.nu**2))
+            self.Q12 = self.nu*self.E/(1-self.nu**2)
               
         if not "Geometry" in yamlfile:
             print "Geometry?"
@@ -45,3 +49,54 @@ class Deck():
             sys.exit(1)
         else:
             self.Step = self.check_presence("DIC", "Step")  
+            self.mmPerPix = self.check_presence("DIC", "mmperpixel")
+            self.SmallArea = (self.Step*self.mmPerPix)**2
+            
+
+class Coordinates():
+    
+    def __init__(self, deck):
+        self.xycoordinates(deck)
+        self.polarcoordinates()
+    
+    def xycoordinates(self, deck):
+        start = -deck.Radius-(deck.Step*deck.mmPerPix/2)
+        stepsize = deck.Step*deck.mmPerPix
+        end = deck.Rad+(deck.Step*deck.mmPerPix/2)  
+        x = []
+        while  start + i * stepsize <= end:
+            x.append(start + i * stepsize)
+            i+=1
+        self.x = x   
+        
+        xMat = np.zeros((len(x),len(x)),dtype=float)
+        yMat = np.zeros((len(x),len(x)),dtype=float)  
+        
+        for i in range(0,len(x)):
+            xMat[:,i] = x[i]
+            yMat[:,i] = x[len(x)-i-1]
+        self.xMat = xMat
+        self.yMat = yMat
+        
+    def cart2pol(self, x, y):
+        rho = np.sqrt(x**2 + y**2)
+        phi = np.arctan2(y, x)
+        return(rho, phi)
+        
+    def polarcoordinates(self):
+        theta = np.zeros((len(self.x),len(self.x)))    
+        r = np.zeros((len(self.x),len(self.x)))
+
+        for i in range(0,len(self.x)):
+            rpart = []
+            thetapart = []
+            for j in range(0,len(self.x)):
+                radius , angle = self.cart2pol(self.x[j],self.x[len(self.x)-i-1])
+                rpart.append(radius)
+                thetapart.append(angle)
+            theta[i,:] = thetapart
+            r[i,:] = rpart
+            
+        r[r > Rad] = 0
+        self.theta = theta
+        self.r = r
--- virtual_field.py
+++ virtual_field.py
@@ -3,68 +3,18 @@ import numpy.ma as ma
 from numpy import linalg
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from utils import *
 
-def cart2pol(x, y):
-    rho = np.sqrt(x**2 + y**2)
-    phi = np.arctan2(y, x)
-    return(rho, phi)
 
 def imagesc(x,y,c):
     plt.imshow(c,extent=(x.min(),x.max(),y.min(),y.max()),interpolation='nearest', cmap=cm.gist_rainbow)
     plt.colorbar()
     plt.show()
 
-E = 200000 #MPa=N/mm^2
-nu = 0.29
+deck = Deck("./deck.yaml")
+coordinates = Coordinates( deck )
 
-P = 7000 #Applied load in Newton
-
-InputQ11 = E/(1-(nu**2))
-InputQ12 = nu*E/(1-nu**2)
-
-# Dimesions of the disk
-Rad = 50. #mm
-t = 2. #mm
-
-# 2D-DIC parameter
-Step = 10.
-mmPerPix = 2*Rad/2000 # mm / pixel ratio
-SmallArea = (Step*mmPerPix)**2
-
-start = -Rad-(Step*mmPerPix/2)
-stepsize = Step*mmPerPix
-end = Rad+(Step*mmPerPix/2)
-i = 0
-x = []
-while  start + i * stepsize <= end:
-    x.append(start + i * stepsize)
-    i+=1
-    
-xMat = np.zeros((len(x),len(x)),dtype=float)
-yMat = np.zeros((len(x),len(x)),dtype=float)
-
-for i in range(0,len(x)):
-    xMat[:,i] = x[i]
-    yMat[:,i] = x[len(x)-i-1]
-    
-theta = np.zeros((len(x),len(x)))    
-r = np.zeros((len(x),len(x)))
-
-for i in range(0,len(x)):
-    rpart = []
-    thetapart = []
-    for j in range(0,len(x)):
-        radius , angle = cart2pol(x[j],x[len(x)-i-1])
-        rpart.append(radius)
-        thetapart.append(angle)
-    theta[i,:] = thetapart
-    r[i,:] = rpart
-    
-reduced = ma.masked_where(r > Rad,r)
-
-
-
-sigma1 = (-2.*P/(np.pi*t))*(np.divide(np.multiply((Rad-yMat),xMat**2),((xMat**2)+(Rad-yMat)**2)**2)+np.divide(np.multiply(Rad+yMat,xMat**2),(xMat**2+(Rad+yMat)**2)**2))
+sigma1 = (-2.*P/(np.pi*t))*(np.divide( np.multiply((Rad-yMat),xMat**2),((xMat**2)+(Rad-yMat)**2)**2 ) + np.divide(np.multiply(Rad+yMat,xMat**2),(xMat**2+(Rad+yMat)**2)**2))
 
 #print ((xMat**2)+(Rad-yMat)**2)**2
 
